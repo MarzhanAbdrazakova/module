5cd2804878bb870f7f188895741e1d3642fd34c2
MarzhanAbdrazakova <atmarzhan@gmail.com>
2017-06-13 20:57:15 +0300

    Create tcp_port.py

--- tcp_port.py
+++ tcp_port.py
@@ -6,10 +6,12 @@ import json
 def rest(tcp1,tcp2,inport,outport):
 
     url = 'http://localhost:8080/forward/rules/0000000000000001'
-    payload = { "dpid": 1, "priority":100, "match": { "nw_dst": "10.0.0.2", "nw_proto": "TCP", "eth_type": 2048, "tcp_dst": tcp1, "in_port": inport}, "actions":  { "tcp_dst": tcp2, "OUTPUT":outport}  }
+    payload = { "dpid": 1, "priority":100, "match": { "nw_dst": "10.0.0.2", "nw_proto": "TCP", "eth_type": 2048, "tcp_dst": tcp1, 
+                                                     "in_port": inport}, "actions":  { "tcp_dst": tcp2, "OUTPUT":outport}  }
     r=requests.post(url, json.dumps(payload))
 
-    payload = { "dpid": 1, "priority":100, "match": { "nw_src": "10.0.0.2", "nw_proto": "TCP", "eth_type": 2048, "tcp_src": tcp2, "in_port": outport}, "actions": { "tcp_src": tcp1, "OUTPUT":inport} }
+    payload = { "dpid": 1, "priority":100, "match": { "nw_src": "10.0.0.2", "nw_proto": "TCP", "eth_type": 2048, "tcp_src": tcp2, 
+                                                     "in_port": outport}, "actions": { "tcp_src": tcp1, "OUTPUT":inport} }
     r=requests.post(url, json.dumps(payload))
     
 def main():
@@ -19,6 +21,5 @@ def main():
     h = input("Enter output port: ")
     rest(x,y,z,h)
     
-
 if __name__ == '__main__':
     main()
